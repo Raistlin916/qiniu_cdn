a978dea79b27d669b1ac490f08a2a9ffd321b8ec
gaok <raistlin916@gmail.com>
2014-12-19 16:58:25 +0800

    improve

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var fs = require('vinyl-fs');
 var qiniuUpload = require('./lib/qiniu_upload');
 
+var posix = require('posix');
+posix.setrlimit('nofile', { soft: 10000, hard: 10000 });
+
 module.exports = function(setting, qiniuConfig){
     return fs.src(setting.src)
             .pipe(qiniuUpload(qiniuConfig, {
