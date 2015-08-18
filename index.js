57d57c939d0e12a18478c38af45346bfb6a8fb81
gaokai <gaokai@qima-inc.comgit>
2015-08-18 14:58:49 +0800

    remove posix

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var fs = require('vinyl-fs');
 var qiniuUpload = require('./lib/qiniu_upload');
 
-var posix = require('posix');
-posix.setrlimit('nofile', { soft: 10000, hard: 10000 });
-
 module.exports = function(setting, qiniuConfig){
     return fs.src(setting.src)
             .pipe(qiniuUpload(qiniuConfig, {
