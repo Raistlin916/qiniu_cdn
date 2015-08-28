012846a9c8605671cdf86e9915bd968361eace3c
gaokai <gaokai@qima-inc.comgit>
2015-08-28 21:49:56 +0800

    添加验证 dir 不为空

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var fs = require('vinyl-fs');
 var qiniuUpload = require('./lib/qiniu_upload');
 
 module.exports = function(setting, qiniuConfig){
+    if (setting.drc == undefined) {
+	console.log('前缀为空，请执行 npm install 升级node_modules!!!!!');
+	return throw new Error('前缀为空，请执行 npm install 升级node_modules!!!!!');
+    }
     return fs.src(setting.src)
             .pipe(qiniuUpload(qiniuConfig, {
                     dir: setting.dir
