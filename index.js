afebd9dbace40ec34ed8079b0879817f3be175cf
gaokai <gaokai@qima-inc.comgit>
2015-08-28 21:53:04 +0800

    fix

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ var fs = require('vinyl-fs');
 var qiniuUpload = require('./lib/qiniu_upload');
 
 module.exports = function(setting, qiniuConfig){
-    if (setting.drc == undefined) {
-	console.log('前缀为空，请执行 npm install 升级node_modules!!!!!');
-	return throw new Error('前缀为空，请执行 npm install 升级node_modules!!!!!');
-    }
-    return fs.src(setting.src)
+    
+	return fs.src(setting.src)
             .pipe(qiniuUpload(qiniuConfig, {
-                    dir: setting.dir
+                    dir: setting.dir || setting.dest
                 }));
 }
