d4404ed713fbae9865e127efd31c0f4b99547127
jinfeng <jinfeng@qima-inc.com>
2015-12-02 14:32:26 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var nodeQiniu = require('node-qiniu');
 var through = require('through2');
 var fs = require('vinyl-fs');
 var Q = require('q');
@@ -6,13 +5,13 @@ var through2Concurrent = require('through2-concurrent');
 var getEtag = require('./lib/etag');
 var util = require('./lib/util');
 var highWaterMark = 1024;
+var Qiniu = require('qiniu');
 
 module.exports = function(upload, auth, callback) {
-    nodeQiniu.config({
-        access_key: auth.accessKey,
-        secret_key: auth.secretKey
-    });
-    var  context = {
+    Qiniu.conf.ACCESS_KEY = auth.accessKey;
+    Qiniu.conf.SECRET_KEY = auth.secretKey;
+
+    var context = {
         needUploadNum: 0, // 需要上传数量
         alreadyUploadNum: 0, // 已上传数量
         modifyFilesNum: 0, // 已上传，但本地修改数量
@@ -21,8 +20,10 @@ module.exports = function(upload, auth, callback) {
         logCheckDefer: Q.defer(),
         uploadDefer: Q.defer(),
         logUploadFailDefer: Q.defer(),
+        auth: auth,
 
-        qiniu: nodeQiniu.bucket(auth.bucket),
+        qiniuRs: new Qiniu.rs.Client(),
+        qiniuIo: Qiniu.io,
 
         errors: []
     };
@@ -76,52 +77,59 @@ function init(upload) {
 function checkRemoteFile(context) {
     return through2Concurrent.obj({highWaterMark: highWaterMark}, function(file, encoding, next) {
         if (file.needCheck) {
-            var assert = context.qiniu.key(file.cdnPath);
-            assert.stat(function(error, result) {
-                if (error) {
-                    context.errorCheckNum++;
+            context.qiniuRs.stat(context.auth.bucket, file.cdnPath, function(rerr, result, res) {
+                if (rerr) {
+                    if (rerr.code === 612) { // 文件不存在或已删除
+                        file.needCheck = false;
+                        file.needUpload = true;
+                        context.needUploadNum++;
 
-                    file.needCheck = true;
-                    file.checkFailMsg = '检查文件出错';
-                    file.checkFailRes = JSON.stringify(error);
-                    file.checkTryCount++;
+                        // 重试错误计算
+                        if (file.checkTryCount > 0) {
+                            context.errorCheckNum--;
+                        }
+                    } else {
+                        file.needCheck = true;
+                        file.checkTryCount++;
+                        file.checkFailMsg = rerr.code;
+                        file.checkFailRes = JSON.stringify(rerr);
+                    }
 
                     pass();
                 } else {
                     getEtag(file.contents, function(hash) {
-                        // 无hash值，cdn上没有文件，待上传
-                        if (!result.hash) {
-                            file.needCheck = false;
-                            file.needUpload = true;
-                            context.needUploadNum++;
-                        }
                         // 本地hash计算出错
-                        else if (!hash) {
+                        if (!hash) {
                             file.needCheck = true;
                             file.checkTryCount++;
-                            context.errorCheckNum++;
+                            file.checkFailMsg = 'HASH出错';
+                            file.checkFailRes = JSON.stringify({file: file.path});
+
+                            if (file.checkTryCount > 0) {
+                                context.errorCheckNum++;
+                            }
                         }
                         // 对比本地hash与cdn上的hash值
                         else {
                             file.needCheck = false;
                             file.needUpload = false;
                             (result.hash == hash) ? context.alreadyUploadNum++ : context.modifyFilesNum++;
+
+                            // 重试错误计算
+                            if (file.checkTryCount > 0) {
+                                context.errorCheckNum--;
+                            }
                         }
 
                         pass();
                     });
                 }
+            })
 
-                function pass() {
-                    // 重试错误计算
-                    if (file.checkTryCount > 1) {
-                        context.errorCheckNum--;
-                    }
-
-                    next(null, file);
-                    util.logCheck(context.alreadyUploadNum + context.modifyFilesNum, context.needUploadNum, context.errorCheckNum);
-                }
-            });
+            function pass() {
+                next(null, file);
+                util.logCheck(context.alreadyUploadNum + context.modifyFilesNum, context.needUploadNum, context.errorCheckNum);
+            }
         } else {
             next(null, file);
         }
@@ -143,7 +151,11 @@ function logCheckFailed(context) {
 function uploadFile(context) {
     return through2Concurrent.obj({highWaterMark: highWaterMark}, function(file, encoding, next) {
         if (file.needUpload) {
-            context.qiniu.putFile(file.cdnPath, file.path, function(error, result) {
+            var token = (new Qiniu.rs.PutPolicy(context.auth.bucket)).token();
+            var extra = new Qiniu.io.PutExtra();
+            extra.checkCrc = 1;
+
+            context.qiniuIo.putFile(token, file.cdnPath, file.path, extra, function(error, result) {
                 file.uploadSuccess = false;
                 if (error) {
                     file.uploadFailMsg = '上传出错';
